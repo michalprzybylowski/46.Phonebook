6f066cd824076e17530feaba218bd7efa67e37e8
Michał Przybyłowski <myss1990@gmail.com>
2023-05-07 15:05:56 +0200

    Switch and method

---
+++ 46.Phonebook/ContactRepository.cs
@@ -0,0 +1,60 @@
+﻿namespace Phonebook
+{
+    internal class ContactRepository
+    {
+        
+        public static void AddNewContact(Dictionary<string, Contact> contacts)
+        {
+            Console.Write("Write your number:");
+            string TeleNumber = Console.ReadLine();
+            Console.Write("Write your contact name:");
+            string ContactName = Console.ReadLine();
+            contacts.TryAdd($"{TeleNumber}", new Contact(ContactName));
+        }
+
+        public static void ShowContact(Dictionary<string, Contact> contacts)
+        {
+            Console.Write("Display contact from phonenumber:");
+            string userInput = Console.ReadLine();
+
+
+
+            Contact selectedContact = null;
+
+            if (contacts.TryGetValue(userInput, out selectedContact))
+            {
+                Console.WriteLine($"Name for this phonenumber is {selectedContact.Name}");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found");
+            }
+
+        }
+        public static void ShowAllContacts(Dictionary<string, Contact> contacts)
+        {
+            foreach (KeyValuePair<string, Contact> contact in contacts)
+            {
+                Console.WriteLine(string.Format("Number: {0}, Name: {1}",
+                    contact.Key, (string)contact.Value));
+            }
+        }
+        public static void SearchContacts(Dictionary<string, Contact> contacts)
+        {
+            Console.Write("Display contact from name:");
+            string userInput = Console.ReadLine();
+
+            Contact selectedPhoneNumber = null;
+            var myKey = contacts.FirstOrDefault(x => x.Value.Name == userInput).Key;
+            if (contacts.ContainsKey(myKey))
+            {
+                Console.WriteLine($"Phonenumber for this phonenumber is {myKey}");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found");
+            }
+        }
+        
+    }
+}
--- 46.Phonebook/Program.cs
+++ 46.Phonebook/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Phonebook
 {
-    internal class Program : AddNewContact
+    public class Program
     {
 
         static Dictionary<string, Contact> GetContact()
@@ -23,81 +23,100 @@ namespace Phonebook
         {
             Dictionary<string, Contact> contacts = GetContact();
 
-            
 
-            while (true)
+            bool done = false;
+            while (!done)
             {
                 Console.WriteLine("Select what do you want to do(Add, ShowAll, ShowContact, SearchContact, Quit ):");
                 string Menu = Console.ReadLine().ToLower();
                 switch (Menu)
                 {
-                    case "add":
-                        addNewContact(contacts);
+                    case "add":                        
+                        ContactRepository.AddNewContact(contacts);
                         break;
-                }
-                    
+                    case "showcontact":
+                        ContactRepository.ShowContact(contacts);
+                        break;
+                    case "showall":
+                        ContactRepository.ShowAllContacts(contacts);
+                        break;
+                    case "searchcontact":
+                        ContactRepository.SearchContacts(contacts);
+                        break;
+                    case "quit":
+                        done = true;
+                        break;
+                    default:
+                        Console.WriteLine("There isn't option for this input");
+                        break;
+                        
+                        
 
-                if (Menu == "add")
-                {
-                    Console.Write("Write your number:");
-                    string TeleNumber = Console.ReadLine();
-                    Console.Write("Write your contact name:");
-                    string ContactName = Console.ReadLine();
-                    contacts.TryAdd($"{TeleNumber}", new Contact(ContactName));
-                }
-                else if (Menu == "showcontact")
-                {
-                    Console.Write("Display contact from phonenumber:");
-                    string userInput = Console.ReadLine();
-
-
-
-                    Contact selectedContact = null;
-
-                    if (contacts.TryGetValue(userInput, out selectedContact))
-                    {
-                        Console.WriteLine($"Name for this phonenumber is {selectedContact.Name}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Contact not found");
-                    }
-                }
-                else if (Menu == "ShowAll")
-                {
-                    foreach (KeyValuePair<string, Contact> contact in contacts)
-                    {
-                        Console.WriteLine(string.Format("Key: {0}, Value: {1}",
-                            contact.Key, (string)contact.Value));
-                    }
-                }
-                else if (Menu == "SearchContact")
-                {
-                    Console.Write("Display contact from name:");
-                    string userInput = Console.ReadLine();
-
-                    Contact selectedPhoneNumber = null;
-                    var myKey = contacts.FirstOrDefault(x => x.Value.Name == userInput).Key;
-                    if (contacts.ContainsKey(myKey))
-                    {
-                        Console.WriteLine($"Phonenumber for this phonenumber is {myKey}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Contact not found");
-                    }
-                    
 
                 }
-                
-                else if (Menu == "Quit")
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("There isnt option for this input");
-                }
+
+
+                //if (Menu == "add")
+                //{
+                //    Console.Write("Write your number:");
+                //    string TeleNumber = Console.ReadLine();
+                //    Console.Write("Write your contact name:");
+                //    string ContactName = Console.ReadLine();
+                //    contacts.TryAdd($"{TeleNumber}", new Contact(ContactName));
+                //}
+                //else if (Menu == "showcontact")
+                //{
+                //    Console.Write("Display contact from phonenumber:");
+                //    string userInput = Console.ReadLine();
+
+
+
+                //    Contact selectedContact = null;
+
+                //    if (contacts.TryGetValue(userInput, out selectedContact))
+                //    {
+                //        Console.WriteLine($"Name for this phonenumber is {selectedContact.Name}");
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("Contact not found");
+                //    }
+                //}
+                //else if (Menu == "showall")
+                //{
+                //    foreach (KeyValuePair<string, Contact> contact in contacts)
+                //    {
+                //        Console.WriteLine(string.Format("Number: {0}, Name: {1}",
+                //            contact.Key, (string)contact.Value));
+                //    }
+                //}
+                //else if (Menu == "searchcontact")
+                //{
+                //    Console.Write("Display contact from name:");
+                //    string userInput = Console.ReadLine();
+
+                //    Contact selectedPhoneNumber = null;
+                //    var myKey = contacts.FirstOrDefault(x => x.Value.Name == userInput).Key;
+                //    if (contacts.ContainsKey(myKey))
+                //    {
+                //        Console.WriteLine($"Phonenumber for this phonenumber is {myKey}");
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("Contact not found");
+                //    }
+
+
+                //}
+
+                //else if (Menu == "quit")
+                //{
+                //    break;
+                //}
+                //else
+                //{
+                //    Console.WriteLine("There isnt option for this input");
+                //}
 
             }
 
